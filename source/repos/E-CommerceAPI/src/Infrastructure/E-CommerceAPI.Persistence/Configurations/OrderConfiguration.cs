2ef943ed71278f9dc08c8608aa881a561429a6b9
Yunis <yunisalizdev@gmail.com>
2025-07-03 01:40:17 +0400

    Added configurations

---
+++ source/repos/E-CommerceAPI/src/Infrastructure/E-CommerceAPI.Persistence/Configurations/OrderConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using E_CommerceAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_CommerceAPI.Persistence.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(o => o.Id);
+
+        builder.HasOne(o => o.Buyer)
+               .WithMany(u => u.Orders)
+               .HasForeignKey(o => o.BuyerId);
+
+        builder.HasOne(o => o.Product)
+               .WithMany(p => p.Orders)
+               .HasForeignKey(o => o.ProductId);
+    }
+}
